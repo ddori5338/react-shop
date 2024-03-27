98e3716adbaf1f8f5fea15cbe1d82375c9972aa8
youngchangyoo <ddori5338@hanmail.net>
2024-03-27 23:53:30 +0900

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+
+const Navbar = () => {
+    const menuList = ['여성', 'Divided', '남성', '신생아/유아', '아동', 'H&M Home', 'Sale', '지속가능성'];
+    return (
+        <div>
+            <div>
+                <div class="login-button">
+                    <FontAwesomeIcon icon={faUser} />
+                    <div>로그인</div>
+                </div>
+            </div>
+            <div className="nav-section">
+                <img width={100} src="https://upload.wikimedia.org/wikipedia/commons/5/53/H%26M-Logo.svg" alt=""/>
+            </div>
+            <div className="menu-area">
+                <ul className="menu-list">
+                    {menuList.map(menu => <li>{menu}</li>)}
+                </ul>
+                <div className="search-box">
+                    <FontAwesomeIcon icon={faSearch} />
+                    <input type="text" placeholder="제품 검색"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
